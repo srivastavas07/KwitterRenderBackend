5e007520292e86d44440ff00d833219a7c9e02fa
Kunal Chandra <72646476+srivastavas07@users.noreply.github.com>
2024-07-26 00:54:36 +0530

    debugging with sharma ji

--- config/auth.js
+++ config/auth.js
@@ -2,16 +2,19 @@ import jwt from "jsonwebtoken";
 const isAuthenticated = async(req,res,next) => {
     try{
         const {token} = req.cookies;
+        console.log("token printing..")
         console.log(token);
         if(!token){
+            console.log("inside if block of isAuth")
             return res.status(401).json({
                 message:"User Unauthorised..!!",
                 success:false,
             })
         }
         const decode = await jwt.verify(token,process.env.TOKEN_SECRET);
+        console.log("decode ke pehle")
         console.log(decode);
-        req.user =decode.id;
+        req.user =decode.userId;
         next();
     }catch(error){
         console.log(error);
